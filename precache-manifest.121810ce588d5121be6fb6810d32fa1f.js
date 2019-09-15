027b63b9d0ae5329e8d43fad1f29f828b2e93216
Temitope <temmieayodele@gmail.com>
2019-09-15 21:57:43 +0100

    Updates

---
+++ precache-manifest.121810ce588d5121be6fb6810d32fa1f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d57423bec76f51b5e5f7e76bcd3f35f",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "27a96ba7a402a4b7a873",
+    "url": "/my-portfolio/static/css/main.3c721538.chunk.css"
+  },
+  {
+    "revision": "86a799069863c2b1e698",
+    "url": "/my-portfolio/static/js/2.09c0daef.chunk.js"
+  },
+  {
+    "revision": "27a96ba7a402a4b7a873",
+    "url": "/my-portfolio/static/js/main.18e875b3.chunk.js"
+  },
+  {
+    "revision": "f2d15970f74596a69ca7",
+    "url": "/my-portfolio/static/js/runtime~main.674e5ed7.js"
+  }
+]);
