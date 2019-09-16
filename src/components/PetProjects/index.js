08185a616606604d9ec4988400d6786d7a625541
Temitope <temmieayodele@gmail.com>
2019-09-16 18:42:41 +0100

    new edits

--- src/components/PetProjects/index.js
+++ src/components/PetProjects/index.js
@@ -1,16 +1,16 @@
-import React, {useState, useEffect} from "react"
-import {projectList} from "./projectList"
+import React, { useState, useEffect } from "react"
+import { projectList } from "./projectList"
 import "./PetProjects.css"
 
 const PetProjects = () => {
     const [petProjects, setPetProjects] = useState([])
-    useEffect(()=>{
+    useEffect(() => {
         setPetProjects(projectList)
     }, [petProjects])
-    const renderProjects = ()=>{
-        return(
-            petProjects.map((project)=>{
-                return(
+    const renderProjects = () => {
+        return (
+            petProjects.map((project) => {
+                return (
                     <article key={project.id}>
                         <h3><a href={project.site_link} target="_blank" rel="noopener noreferrer">{project.name}</a></h3>
                         <p>{project.description}</p>
@@ -20,10 +20,10 @@ const PetProjects = () => {
                 )
             })
         )
-    } 
+    }
     return (
         <div className="pet_projects">
-        <h1>MY PROJECTS</h1>
+            <h1>MY PROJECTS</h1>
             <section>
                 {petProjects.length > 0 && renderProjects()}
             </section>
