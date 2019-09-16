89e5ab2553905d4f9592064d8574330823929360
Temitope <temmieayodele@gmail.com>
2019-09-16 21:21:32 +0100

    Updates

---
+++ precache-manifest.79bf09c9c6f7aa235c45ed5add62285b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c29054e0a73ac5465f9c62c4d62a487",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b9b8f58809115b8c771b",
+    "url": "/portfolio/static/css/main.a8fc375a.chunk.css"
+  },
+  {
+    "revision": "86a799069863c2b1e698",
+    "url": "/portfolio/static/js/2.09c0daef.chunk.js"
+  },
+  {
+    "revision": "b9b8f58809115b8c771b",
+    "url": "/portfolio/static/js/main.2c251825.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-portfolio/precache-manifest.926e6cd56c047b96e15a5af871754564.js"
+  "/portfolio/precache-manifest.79bf09c9c6f7aa235c45ed5add62285b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
