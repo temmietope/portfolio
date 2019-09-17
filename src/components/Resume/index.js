7c048af17c8a1ea762d95270dfcc48b2bb1e3f90
Temitope <temmieayodele@gmail.com>
2019-09-17 06:36:56 +0100

    ui edit

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -4,9 +4,9 @@ import "./Resume.css"
 const Resume = () => {
     return (
         <div className="resume">
-            <h1>My Resume</h1>
+            <h1>MY RESUME</h1>
             <section>
-                <a href="./file/Temitope_resume.pdf" download> <img src="./images/temitope-ayodele-resume.JPG" alt="my-resume" /></a>
+                <a href="./file/Temitope_Ayodele.pdf" download> <img src="./images/temitope-ayodele-resume.JPG" alt="my-resume" /></a>
             </section>
         </div>
     )
