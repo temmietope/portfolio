313e96c25ae42ed177e6d9a898722d0ba47f7201
Temitope <temmieayodele@gmail.com>
2019-09-17 06:39:19 +0100

    ui edit

--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -8,7 +8,7 @@ const Welcome = (props) => {
             <header>
                 <img src="./images/carouselA.jpg" alt="carousel" />
                 <span>
-                    <h1>Hi! My name isTEMITOPE AYODELE</h1>
+                    <h1>Hi! My name is TEMITOPE AYODELE</h1>
                     <p>I am a frontend developer</p>
                 </span>
             </header>
