835069eeefb9485c4752a82256b0bf429e393ea3
Temitope <temmieayodele@gmail.com>
2019-09-22 20:44:03 +0100

    new entry

--- src/components/PetProjects/projectList.js
+++ src/components/PetProjects/projectList.js
@@ -10,21 +10,29 @@ export const projectList = [
     {
         id: "2",
         name: "StarWars Directory",
-        site_link: "",
+        site_link: "https://github.com/temmietope/starwars-directory",
         description: "An app that uses the StarWars API to provide infomation about Starwars Planets, Characters and Starships.",
         tech_stack: "VueJS",
         repo_link: "https://github.com/temmietope/starwars-directory"
     },
     {
         id: "3",
+        name: "Employee Dashboard",
+        site_link: "https://employeedashboardd.netlify.com/",
+        description: "Employee dashboard ui built using the Github users API",
+        tech_stack: "ReactJS",
+        repo_link: "https://github.com/temmietope/dashboard-ui"
+    },
+    {
+        id: "4",
         name: "Firebase-React Authentication",
-        site_link: "",
+        site_link: "https://github.com/temmietope/firebase-react-authentication",
         description: "User authentication app built with React and JWT for auth.",
         tech_stack: "Firebase, ReactJS",
         repo_link: "https://github.com/temmietope/firebase-react-authentication"
     },
     {
-        id: "4",
+        id: "5",
         name: "Firebase-React Todo-List Application",
         site_link: "https://todolist-track.netlify.com/",
         description: "App that allows the user write a todo and save in a database. The user can mark the todo when completed and can also delete the todo entry.",
@@ -32,7 +40,7 @@ export const projectList = [
         repo_link: "https://github.com/temmietope/firebase-react-todo"
     },
     {
-        id: "5",
+        id: "6",
         name: "My Portfolio",
         site_link: "temmietope.github.io",
         description: "My Personal Portfolio",
@@ -40,7 +48,7 @@ export const projectList = [
         repo_link: "https://github.com/temmietope/temmietope.github.io"
     },
     {
-        id: "6",
+        id: "7",
         name: "Quiz App",
         site_link: "https://quizzie.netlify.com/",
         description: "Loads question from an inbuilt JSON file and returns scores after quiz is completed",
@@ -48,7 +56,7 @@ export const projectList = [
         repo_link: "https://github.com/temmietope/quiz-app-with-reactjs"
     },
     {
-        id: "7",
+        id: "8",
         name: "Truth or Dare",
         site_link: "https://truth-or-dare.netlify.com/",
         description: "Loads truth or dare challenges from an inbuilt JSON file. Good for a home party, or picnic.",
@@ -56,7 +64,7 @@ export const projectList = [
         repo_link: "https://github.com/temmietope/truth-or-dare"
     },
     {
-        id: "8",
+        id: "9",
         name: "Music Player",
         site_link: "https://tender-banach-4bd859.netlify.com/",
         description: "An audio player built with Vanilla JavaScript that plays songs by Nigerian artistes gotten from GidiLounge's Top 10 API",
@@ -64,7 +72,7 @@ export const projectList = [
         repo_link: "https://github.com/temmietope/Music-player"
     },
     {
-        id: "9",
+        id: "10",
         name: "Emoji Guess Game",
         site_link: "https://laughing-ritchie-f7d3ea.netlify.com/",
         description: "A game that allows user click on two different boxes and it checks if the emojis in both boxes are the same.",
@@ -72,9 +80,9 @@ export const projectList = [
         repo_link: "https://github.com/temmietope/emoji-guess-game"
     },
     {
-        id: "10",
+        id: "11",
         name: "React Authentication",
-        site_link: "",
+        site_link: "https://github.com/temmietope/react-authentication",
         description: "User authentication app built with React and JWT for auth.",
         tech_stack: "ReactJS",
         repo_link: "https://github.com/temmietope/react-authentication"
